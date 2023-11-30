e50384ac2c106f31d698e56960eea3e984430342
SimranjotKaurSandhu <simranjot1372.be21@chitkara.edu.in>
2023-11-30 07:38:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ Recipe_MERN/server/src/index.js
@@ -0,0 +1,47 @@
+// import express from "express";
+// import cors from "cors";
+// import mongoose from "mongoose";
+
+// import {userRouter} from './routes/users.js'
+// import { recipesRouter } from "./routes/recipes.js";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/auth",userRouter);
+// app.use("/recipes",recipesRouter);
+
+// mongoose.connect(
+//     "mongodb+srv://simranjot:MERNpassword123@recipes.gdy5wxt.mongodb.net/recipes?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     }
+// );
+
+// app.listen(3001, () => console.log("Server started"));
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://user123:Password123Tech@test.m6cb1nv.mongodb.net/recipetest?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
